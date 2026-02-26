2d1d06da7a280dd3ed941acf3285d2f4e1c7e1d7
MJB9595 <wansja7870@gmail.com>
2026-02-26 11:23:17 +0900

    Original -> figma

---
+++ src/util/insta.js
@@ -0,0 +1,44 @@
+const img = (path) => `/img/${path}`;
+
+const insta = [
+  {
+    id: 1,
+    image: img("instar1.jpg"),
+    alt: "인스타 이미지 1",
+    link: "/insta1",
+  },
+  {
+    id: 2,
+    image: img("instar2.jpg"),
+    alt: "인스타 이미지 2",
+    link: "/insta2",
+  },
+  {
+    id: 3,
+    image: img("instar3.jpg"),
+    alt: "인스타 이미지 3",
+    link: "/insta3",
+  },
+  {
+    id: 4,
+    image: img("instar4.jpg"),
+    alt: "인스타 이미지 4",
+    link: "/insta4",
+  },
+    {
+    id: 5,
+    image: img("instar5.jpg"),
+    alt: "인스타 이미지 5",
+    link: "/insta5",
+  },
+    {
+    id: 6,
+    image: img("instar6.jpg"),
+    alt: "인스타 이미지 6",
+    link: "/insta6",
+  },
+];
+
+
+  export default insta
+  
