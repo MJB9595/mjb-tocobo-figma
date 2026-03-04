8f258aa7960b2fa958da3fed019e8e68c91d3413
MJB9595 <wansja7870@gmail.com>
2026-03-04 20:16:00 +0900

    Follow 섹션 최종 리파인 및 footer 재조립

--- src/util/footer.js
+++ src/util/footer.js
@@ -6,23 +6,23 @@ import { RiKakaoTalkFill } from "react-icons/ri";
 // 로고
 export const logoData = {
   src: img("logo.svg"),
-  alt: "TOCOBO 로고",
+  alt: "백세주 로고",
   href: "/",
 };
 
 // 회사 정보
 export const companyData = [
-  "주식회사 픽톤  대표 : 이병훈",
-  "주소 : 03966 서울특별시 마포구 월드컵북로15길 45 (성산동)",
-  "사업자등록번호 : 196-81-02410 [사업자정보확인]",
-  "통신판매업신고 : 제2021-서울마포-2994호 개인정보보호책임자 : 안주영",
-  "TEL : 02-6352-2410  E-MAIL : help@thepicton.com",
-  "마케팅문의 : 마케팅팀 mkt@thepicton.com",
-  "수출문의 : 해외영업팀 sales1@thepicton.com",
-  "특판&대량구매문의 : 국내영업팀 sales@thepicton.com",
+  "주식회사 XX | 대표 : XXX",
+  "주소 : 0XX6X 마고특별시 산나비 5길 45 (신수동)",
+  "사업자등록번호 : 323-5499-4771  [사업자정보확인]",
+  "통신판매업신고 : 제2037-마고특별-6294호 개인정보보호책임자 : XXX",
+  "TEL : 05 - 8372 - 9520 | E-MAIL : help@beckseju.com",
+  "마케팅문의 : 마케팅팀 mkt@bekseju.com",
+  "수출문의 : 해외영업팀 sales1@bekseju.com",
+  "특판&대량구매문의 : 국내영업팀 sales@bekseju.com",
 ];
 export const footerLegal = {
-  copyright: "COPYRIGHT© 주식회사 픽톤 2021 TOCOBO. ALL RIGHT RESERVED.",
+  copyright: "COPYRIGHT© 주식회사 MAGO 2026 BEKSEJU ALL RIGHT RESERVED.",
   links: [
     { label: "이용약관", href: "/terms" },
     { label: "개인정보처리방침", href: "/privacy" },
@@ -32,7 +32,7 @@ export const footerLegal = {
 // 고객센터
 export const customerCenterData = {
   title: "Customer Center",
-  tel: { value: "02-6352-2410", href: "tel:0263522410" },
+  tel: { value: "03-2214-3322", href: "tel:0263522410" },
   hours: "AM 10:00 - PM 5:00 (Lunch 12:00 - 13:00)",
   notice: "토, 일요일 및 공휴일 휴무",
   talk: { label: "1:1 TALK", href: "#" },
@@ -41,34 +41,28 @@ export const customerCenterData = {
 // 메뉴
 export const footerMenus = [
   {
-    title: "Brand",
+    title: "STORY",
     items: [
-      { label: "Brand Story", href: "/brand-story" },
-      { label: "Collection", href: "/collection" },
+      { label: "History", href: "/brand-story" },
+      { label: "Provenience", href: "/collection" },
     ],
   },
   {
-    title: "Shop",
+    title: "CRAFT",
     items: [
       { label: "All Product", href: "/shop" },
-      { label: "MakeUp", href: "/shop/makeup" },
-      { label: "Cleansing", href: "/shop/cleansing" },
-      { label: "Skincare", href: "/shop/skincare" },
-      { label: "Sun", href: "/shop/sun" },
-      { label: "Lip", href: "/shop/lip" },
-      { label: "Set/Etc", href: "/shop/etc" },
+      { label: "Ingredient", href: "/shop/makeup" },
     ],
   },
   {
-    title: "Promotion",
-    items: [{ label: "Promotion", href: "/promotion" }],
+    title: "TASTE",
+    items: [{ label: "Reviews", href: "/promotion" }],
   },
   {
-    title: "Community",
+    title: "NEWS",
     items: [
       { label: "Notice", href: "/notice" },
       { label: "FAQ", href: "/faq" },
-      { label: "Review", href: "/review" },
       { label: "1:1 CS", href: "/cs" },
     ],
   },
